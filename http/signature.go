2204ac1a0bc070a47439fdf28a29ff8f43786c79
Joe <sunfuze.1989@gmail.com>
2023-07-14 16:30:16 +0800

    fix: should not override x-timestamp

--- http/signature.go
+++ http/signature.go
@@ -10,10 +10,17 @@ import (
 	"github.com/longbridgeapp/openapi-go/internal/util"
 )
 
+const (
+	headerTimestamp = "x-timestamp"
+)
+
 var sign = &signer.Signer{}
 
 func signature(req *nhttp.Request, secret string, body []byte) error {
-	req.Header.Add("x-timestamp", strconv.FormatInt(time.Now().Unix(), 10))
+	if v := req.Header.Get(headerTimestamp); v == "" {
+		req.Header.Add("x-timestamp", strconv.FormatInt(time.Now().UnixMilli(), 10))
+	}
+
 	req.Header.Add("x-api-signature", "HMAC-SHA256 SignedHeaders=authorization;x-api-key;x-timestamp")
 	signstr, _, _, err := sign.Sign(context.Background(), util.UnsafeStringToBytes(secret), req, body)
 	if err != nil {
