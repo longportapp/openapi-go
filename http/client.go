2f4de9be3013b5d52827c0008ad0a85478982888
Joe <sunfuze.1989@gmail.com>
2024-05-21 17:58:38 +0800

    ensure can debug log response body

--- http/client.go
+++ http/client.go
@@ -159,8 +159,12 @@ func (c *Client) Call(ctx context.Context, method, path string, queryParams inte
 		return err
 	}
 	log.Debugf("http call response headers:%v", httpResp.Header)
-
 	defer httpResp.Body.Close()
+	if rb, err = io.ReadAll(httpResp.Body); err != nil {
+		return err
+	}
+	log.Debugf("http call response body:%s", rb)
+
 	apiResp := &apiResponse{}
 
 	if v := httpResp.Header.Get("x-trace-id"); v != "" {
@@ -168,7 +172,7 @@ func (c *Client) Call(ctx context.Context, method, path string, queryParams inte
 	}
 
 	if isJSON(httpResp.Header.Get("content-type")) {
-		if err = jsonUnmarshal(httpResp.Body, apiResp); err != nil {
+		if err = jsonUnmarshal(bytes.NewReader(rb), apiResp); err != nil {
 			return err
 		}
 	} else {
@@ -176,7 +180,6 @@ func (c *Client) Call(ctx context.Context, method, path string, queryParams inte
 			return err
 		}
 		apiResp.Message = string(rb)
-		log.Debugf("http call response body:%v", string(rb))
 	}
 
 	if httpResp.StatusCode != nhttp.StatusOK || apiResp.Code != 0 {
