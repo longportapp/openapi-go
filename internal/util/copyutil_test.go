87ecaad42d4f802eca3f3e9393605ec1768e59e7
白泽 <patrickjiang0530@gmail.com>
2024-10-14 15:14:19 +0800

    Fix EstimateMaxPurchaseQuantity not return value (#54)
    Fix #50

---
+++ internal/util/copyutil_test.go
@@ -0,0 +1,53 @@
+package util_test
+
+import (
+	"testing"
+
+	"github.com/longbridgeapp/assert"
+	"github.com/longportapp/openapi-go/internal/util"
+
+	"github.com/shopspring/decimal"
+)
+
+func TestCopy(t *testing.T) {
+	fv := []*From{{
+		Price:  "0.1",
+		Price1: "a",
+		Price2: "1.1",
+		Price3: "1a",
+		Num:    "",
+		Num1:   "12",
+	}}
+	price, err := decimal.NewFromString("0.1")
+	assert.NoError(t, err)
+	price2 := decimal.NewFromFloat(1.1)
+	expectedTv := To{
+		Price:  &price,
+		Price1: &decimal.Decimal{},
+		Price2: price2,
+		Price3: decimal.Decimal{},
+		Num1: 12,
+	}
+	var tv To
+	err = util.Copy(&tv, fv[0])
+	assert.NoError(t, err)
+	assert.Equal(t, expectedTv, tv)
+}
+
+type From struct {
+	Price  string
+	Price1 string
+	Price2 string
+	Price3 string
+	Num    string
+	Num1   string
+}
+
+type To struct {
+	Price  *decimal.Decimal
+	Price1 *decimal.Decimal
+	Price2 decimal.Decimal
+	Price3 decimal.Decimal
+	Num    int64
+	Num1   int64
+}
